5c1ccf5f25e25b8ce4a3079d53992dc91c078286
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2025-09-22 20:19:17 +0530

    Delete my-app directory

--- my-app/src/app/providers.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthProvider } from "../context/AuthProvider";
-
-export default function Providers({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
--- my-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-gray-300 py-6 text-center">
-      <p>© {new Date().getFullYear()} GuideLink. All rights reserved.</p>
-    </footer>
-  );
-}
